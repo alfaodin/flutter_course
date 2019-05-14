d74c5f3e6e73802d59c57b95d4d910d271f29fb4
Christian Cartes <alfaodin@gmail.com>
2019-05-14 13:50:03 -0500

    Llamada a un REST

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:hello_world/product_manager.dart';
 
 main() {
@@ -10,16 +14,103 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      title: 'test',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
         cardColor: Color.fromARGB(140, 255, 0, 0),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World'),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: Text('Hello World'),
+      //   ),
+      //   body: ProductManager(['test']),
+      // ),
+      home: PhotoList(),
+    );
+  }
+}
+
+class PhotoList extends StatefulWidget {
+  @override
+  PhotoListState createState() => PhotoListState();
+}
+
+class PhotoListState extends State<PhotoList> {
+  StreamController<Photo> streamController;
+  List<Photo> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    streamController = StreamController.broadcast();
+
+    streamController.stream.listen((p) => setState(() => list.add(p)));
+
+    load(streamController);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    streamController?.close();
+    streamController = null;
+  }
+
+  load(StreamController sc) async {
+    String url = 'https://jsonplaceholder.typicode.com/photos';
+    var client = new http.Client();
+    var req = new http.Request('get', Uri.parse(url));
+
+    var streamResp = await client.send(req);
+
+    streamResp.stream
+        .transform(utf8.decoder)
+        .transform(json.decoder)
+        .expand((e) => e)
+        .map((map) => Photo.fromJsonMap(map))
+        .pipe(streamController);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stramed photos'),
+      ),
+      body: Center(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) => _makeElement(index),
         ),
-        body: ProductManager(['test']),
       ),
     );
   }
+
+  Widget _makeElement(int index) {
+    if (index >= list.length) {
+      return null;
+    }
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: FittedBox(
+        fit: BoxFit.fitHeight,
+        child: Column(
+          children: <Widget>[
+            Image.network(list[index].url),
+            Text(list[index].title)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Photo {
+  final String title;
+  final String url;
+
+  Photo.fromJsonMap(Map map)
+      : title = map['title'],
+        url = map['url'];
 }
