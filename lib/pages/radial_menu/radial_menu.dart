a24e76d8533fcf276b650fa9e8a609920c7f3109
Christian Cartes <alfaodin@gmail.com>
2019-12-05 22:26:02 -0500

    Menu radial completo

--- lib/pages/radial_menu/radial_menu.dart
+++ lib/pages/radial_menu/radial_menu.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:math' as math;
 
-class RadialMenu extends StatelessWidget {
+class MenuOption {
+  final String label;
+  final IconData iconData;
+
+  MenuOption(this.label, this.iconData);
+}
+
+class RadialMenu extends StatefulWidget {
   const RadialMenu({Key key}) : super(key: key);
+
+  @override
+  _RadialMenuState createState() => _RadialMenuState();
+}
+
+class _RadialMenuState extends State<RadialMenu> with TickerProviderStateMixin {
+  final List<MenuOption> _options = [];
+  AnimationController _controller;
+  Animation<double> _animationAngle;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _options.add(MenuOption('Menu 1', Icons.label));
+    _options.add(MenuOption('Menu 2', Icons.label_important));
+    _options.add(MenuOption('Menu 3', Icons.laptop_chromebook));
+    _options.add(MenuOption('Menu 4', Icons.layers));
+    _options.add(MenuOption('Menu 5', Icons.library_music));
+    _options.add(MenuOption('Menu 6', Icons.link_off));
+    _controller = AnimationController(
+      duration: Duration(
+        milliseconds: 500,
+      ),
+      
+      vsync: this,
+    );
+
+    _animationAngle = Tween<double>(
+      begin: -math.pi,
+      end: 0,
+    ).animate(_controller,);
+    _animationAngle.addListener(() {
+      setState(() {});
+    });
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final decoratorWidth = size.width * .85;
     final containerOuterWidth = decoratorWidth + 100;
+
+    final itemsCount = _options.length;
+    final step = math.pi / itemsCount;
 
     return Scaffold(
       body: Container(
@@ -24,22 +79,48 @@ class RadialMenu extends StatelessWidget {
                 children: <Widget>[
                   Positioned(
                     right: -decoratorWidth / 2,
-                    child: SvgPicture.asset(
-                      'assets/radial_menu/decoration.svg',
-                      width: decoratorWidth,
+                    child: Transform.rotate(
+                      angle: _animationAngle.value,
+                      child: SvgPicture.asset(
+                        'assets/radial_menu/decoration.svg',
+                        width: decoratorWidth,
+                      ),
                     ),
                   ),
                   Positioned(
                     right: -containerOuterWidth / 2,
-                    child: Container(
-                      width: containerOuterWidth,
-                      height: containerOuterWidth,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          width: 2,
-                          color: Colors.redAccent,
-                        ),
+                    child: Transform.rotate(
+                      angle: _animationAngle.value,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: containerOuterWidth,
+                            height: containerOuterWidth,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 2,
+                                color: Colors.redAccent,
+                              ),
+                            ),
+                          ),
+                          ...List.generate(itemsCount, (int index) {
+                            final angle =
+                                (math.pi / 2 + index * step) + step / 2;
+                            return MenuItem(
+                              menuOption: _options[index],
+                              offset: Offset(
+                                containerOuterWidth / 2 * math.cos(angle),
+                                containerOuterWidth / 2 * math.sin(angle),
+                              ),
+                              textOffset: Offset(
+                                -60,
+                                10 - (30 * (index / itemsCount)),
+                              ),
+                            );
+                          })
+                        ],
                       ),
                     ),
                   )
@@ -100,6 +181,57 @@ class ToolBar extends StatelessWidget {
                   )),
             ),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final Offset offset;
+  final Offset textOffset;
+  final MenuOption menuOption;
+
+  const MenuItem(
+      {Key key,
+      @required this.menuOption,
+      @required this.offset,
+      @required this.textOffset})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: offset,
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          CupertinoButton(
+            onPressed: () {},
+            padding: EdgeInsets.zero,
+            child: Container(
+              width: 50,
+              height: 50,
+              child: Center(
+                child: Icon(
+                  menuOption.iconData,
+                ),
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.redAccent,
+              ),
+            ),
+          ),
+          Transform.translate(
+            offset: textOffset,
+            child: SizedBox(
+              width: 100,
+              child: Text(
+                menuOption.label,
+              ),
+            ),
+          ),
         ],
       ),
     );
