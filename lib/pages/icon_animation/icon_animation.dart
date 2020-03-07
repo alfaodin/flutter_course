9044800b1e251446c684b205a24f3a595e1cfca6
Christian Cartes <alfaodin@gmail.com>
2020-03-07 08:43:59 -0500

    @CCartes perlin noise

--- lib/pages/icon_animation/icon_animation.dart
+++ lib/pages/icon_animation/icon_animation.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:fast_noise/fast_noise.dart';
 import 'package:flutter/material.dart';
 
 class IconAnimation extends StatefulWidget {
@@ -8,7 +11,7 @@ class IconAnimation extends StatefulWidget {
 }
 
 class _IconAnimationState extends State<IconAnimation>
-    with SingleTickerProviderStateMixin  {
+    with SingleTickerProviderStateMixin {
   double iconSize = 0.0;
   double maxIconSize = 20;
   Animation<double> _animation;
@@ -18,8 +21,8 @@ class _IconAnimationState extends State<IconAnimation>
   void initState() {
     super.initState();
 
-    _controller =
-        AnimationController(duration: const Duration(milliseconds: 600), vsync: this);
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 600), vsync: this);
   }
 
   @override
@@ -43,16 +46,21 @@ class _IconAnimationState extends State<IconAnimation>
 
   Widget _content() {
     return SafeArea(
-      child: Center(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          color: Colors.blueGrey,
-          child: Icon(
-            Icons.insert_emoticon,
-            size: 30 + iconSize,
+      child: Stack(
+        children: <Widget>[
+          PixelNoise(
+            color: Colors.deepOrangeAccent,
           ),
-        ),
+          Center(
+            child: Container(
+              color: Colors.blueGrey,
+              child: Icon(
+                Icons.insert_emoticon,
+                size: 30 + iconSize,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -73,5 +81,88 @@ class _IconAnimationState extends State<IconAnimation>
         });
       });
     _controller.forward();
+  }
+}
+
+class PixelNoise extends StatefulWidget {
+  final Color color;
+
+  PixelNoise({Key key, this.color = Colors.white}) : super(key: key);
+
+  @override
+  _PixelNoiseState createState() => _PixelNoiseState();
+}
+
+class _PixelNoiseState extends State<PixelNoise>
+    with SingleTickerProviderStateMixin {
+  PerlinNoise _perlinNoise;
+
+  @override
+  void initState() {
+    var random = Random();
+
+    _perlinNoise = PerlinNoise(
+      octaves: 4,
+      frequency: 0.15,
+      seed: random.nextInt(1337),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: widget.color,
+      child: CustomPaint(
+        painter: NoisePointer(
+          _perlinNoise,
+          color: Colors.deepPurpleAccent,
+          pixelSize: 20.0,
+        ),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      ),
+    );
+  }
+}
+
+class NoisePointer extends CustomPainter {
+  final Color color;
+  final double pixelSize;
+  final double blockSize;
+  final PerlinNoise _perlinNoise;
+
+  static const _max = sqrt1_2;
+  static const _min = -_max;
+
+  NoisePointer(this._perlinNoise,
+      {@required this.color, @required this.pixelSize})
+      : this.blockSize = pixelSize * 1.4;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Random random = Random();
+
+    Paint paint = Paint()..style = PaintingStyle.fill;
+
+    for (var x = 0.0; x < size.width / blockSize; x++) {
+      for (var y = 0.0; y < size.height / blockSize; y++) {
+        var noise = _perlinNoise.getPerlin2(x, y);
+        var percentage = (noise - _min) / (_max - _min);
+        var rgb = percentage * 255;
+
+        canvas.drawRect(
+            Rect.fromLTWH(x * blockSize, y * blockSize, pixelSize, pixelSize),
+            paint..color = color.withAlpha(rgb.floor()));
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
   }
 }
